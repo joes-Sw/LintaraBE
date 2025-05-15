5cd0693e29f04ee7280302db02f0bf0b4af68d70
joes <joeswaruwu@gmail.com>
2025-05-16 00:00:45 +0700

    index.js changes

--- api/index.js
+++ api/index.js
@@ -31,6 +31,4 @@ app.use(`${API_BASE}/comment`, commentRouter);
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 
-app.listen(5000, () => console.log("Server ready on port 5000."));
-
 module.exports = app;
